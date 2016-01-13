55f83011e32095a7fad9c1fe8c7e07a696941e17
n3phi1im <christopher.johnkellogg@gmail.com>
2016-01-13 09:08:33 -0800

    First Commit

---
+++ public/javascript/app.js
@@ -0,0 +1,15 @@
+'use strict';
+var App;
+(function (App) {
+    angular.module('app', ['ngRoute', 'ngResource'])
+        .config(function ($routeProvider, $locationProvider, $httpProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '/templates/Home.html',
+            controller: app.Controllers.HomeController,
+            controllerAs: 'vm'
+        })
+            .otherwise({ redirectTo: '/' });
+        $locationProvider.html5Mode(true);
+        $httpProvider.interceptors.push('HTTPFactory');
+    });
+})(App || (App = {}));
